90658f46dd71468308ca156d1c06d7dd65ad22d8
motzzki <tizontimothy96@gmail.com>
2024-05-31 11:13:11 +0800

    2nd commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
--- my-project/src/Components/Signup.jsx
+++ my-project/src/Components/Signup.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <form className="bg-white p-6 rounded shadow-md w-full max-w-sm">
-        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
         <div className="mb-4">
           <input
             type="text"
             id="username"
             placeholder="Username"
+            className="mt-2 p-2 w-full border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
             className="mt-2 p-2 w-full border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
@@ -26,13 +34,13 @@ const Login = () => {
           type="submit"
           className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-          Login
+          Sign up
         </button>
 
         <span className="block p-2 text-center">
-          Don't have an account?{" "}
-          <Link to="/signup" className="underline text-cyan-600">
-            Sign Up
+          Already have an account?{" "}
+          <Link to="/" className="underline text-cyan-600">
+            Login
           </Link>
         </span>
       </form>
@@ -40,4 +48,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
